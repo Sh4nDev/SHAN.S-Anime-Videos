2233a6634a5fb62cc6b3e2d6aebe7c3fa9ef2148
SH AN <sh4n.dev@gmail.com>
2025-11-30 02:24:29 +0600

    Update video import path and change endpoint

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const videos = require('Sh4n/videos');
+const videos = require('./Sh4n/videos.json');
 const SHAN = express();
 
 SHAN.use(express.json());
 
-SHAN.get('/c', (req, res) => {
+SHAN.get('/random-video', (req, res) => {
   const ShaN = cute[Math.floor(Math.random() * cute.length)];
   res.json({ 
     author: "♡︎ 𝗦𝗵𝗔𝗻 ♡︎",
